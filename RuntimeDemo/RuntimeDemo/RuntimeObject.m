493797db4aac1e0214193f96d5165afbdec2e03b
郭晓倩 <guoxiaoqian@changingedu.com>
2017-07-28 17:39:48 +0800

    创建项目

---
+++ RuntimeDemo/RuntimeDemo/RuntimeObject.m
@@ -0,0 +1,17 @@
+//
+//  RuntimeObject.m
+//  RuntimeDemo
+//
+//  Created by 郭晓倩 on 2017/7/28.
+//  Copyright © 2017年 郭晓倩. All rights reserved.
+//
+
+#import "RuntimeObject.h"
+
+@implementation RuntimeObject
+
+- (void)method{
+    [RuntimeObject new];
+}
+
+@end
